6a43afa97d7909d0d0d467a254552e1d4d161b3b
skaisanlahti <skaisanlahti@gmail.com>
2020-06-06 11:51:14 +0300

    Refactor grid function dataflows

--- app.js
+++ app.js
@@ -8,7 +8,6 @@
 /*=== DOM Elements ===
 =============================================================================*/
 const gridContainer = document.querySelector(".eas-main__content");
-const gridStyle = document.querySelector(".eas-main__content").style;
 const newButton = document.querySelector(".eas-main__button--new");
 const clearButton = document.querySelector(".eas-main__button--clear");
 const colorButton = document.querySelector(".eas-main__button--color");
@@ -17,25 +16,28 @@ const colorPreview = document.querySelector(".eas-color-preview");
 
 /*=== Variables ===
 =============================================================================*/
-const gridDefault = 64;
-let gridResolution = 64;
+const gridDefaultColumns = 64;
+let gridColumns = gridDefaultColumns;
 let color = "black";
 let random = false;
 
 /*=== Event Listeners ===
 =============================================================================*/
 newButton.addEventListener("click", setNewGrid);
-clearButton.addEventListener("click", createGrid);
+clearButton.addEventListener("click", refreshGrid);
 colorButton.addEventListener("click", setColor);
 randomButton.addEventListener("click", setRandom);
-window.addEventListener("resize", createGrid);
+window.addEventListener("resize", refreshGrid);
 
-/*=== Button functions ===
+/*=== Event functions ===
 =============================================================================*/
 function setNewGrid(){
-    gridResolution = prompt("Enter grid columns:");
-    if (gridResolution === "" || gridResolution === null) gridResolution = gridDefault;
-    createGrid();
+    gridColumns = prompt("Enter grid columns:");
+    if (gridColumns === "" || gridColumns === null) gridColumns = gridDefaultColumns;
+    createGrid(gridContainer, gridColumns);
+}
+function refreshGrid(){
+    createGrid(gridContainer, gridColumns);
 }
 function setColor(){
     color = prompt("Enter color as css string (rgb/hsl/hex):");
@@ -51,6 +53,10 @@ function setRandom(){
     random = !random;
     if (random) randomButton.textContent = "Random: ON";
     else randomButton.textContent = "Random: OFF";
+}
+function changeCellColor(){
+    if (random) this.style.backgroundColor = getRandomHSL();
+    else this.style.backgroundColor = color;
 }
 
 /*=== Helpers ===
@@ -70,43 +76,37 @@ function getRandomHSL(){
     let l = getRandomInteger(0,100);
     return `hsl(${h}, ${s}%, ${l}%)`;
 }
-function changeCellColor(){
-    if (random) this.style.backgroundColor = getRandomHSL();
-    else this.style.backgroundColor = color;
-}
 
 /*=== Grid creation ===
-- Remove old cells if they exist to refresh grid
-- Get container dimensions to make grid adjust to arbitrary container size
-- Add grid styles to container to define columns and rows
-- Create cells
 =============================================================================*/
-function createGrid() {
-    while (gridContainer.firstChild) {
-        gridContainer.removeChild(gridContainer.firstChild);
+function createGrid(container, columns) {
+    let gridDimensions = getGridDimensions(container, columns);
+    styleGridContainer(container, gridDimensions);
+    createGridItems(container, gridDimensions.cells);
+}
+function getGridDimensions(container, columns){
+    let gridInfo = container.getBoundingClientRect();
+    let rows = Math.floor(gridInfo.height/gridInfo.width*columns);
+    let cells = columns * rows;
+    console.log(`columns: ${columns}, rows: ${rows}, cells: ${cells}`);
+    return {columns: columns, rows: rows, cells: cells};
+}
+function styleGridContainer(container, gridDimensions){
+    container.style.gridTemplateColumns = `repeat(${gridDimensions.columns}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridDimensions.rows}, 1fr)`;
+}
+function createGridItems(container, cells){
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
-    
-    let gridInfo = gridContainer.getBoundingClientRect();
-    let gridHeight = gridInfo.height
-    let gridWidth = gridInfo.width;
-
-    let columns = gridResolution;
-    let rows = Math.floor(gridHeight/gridWidth*columns);
-    let cellCount = columns * rows;
-    
-    console.log(`columns: ${columns}, rows: ${rows}, cells: ${cellCount}`);
-    
-    gridStyle.gridTemplateColumns = `repeat(${columns}, 1fr)`;
-    gridStyle.gridTemplateRows = `repeat(${rows}, 1fr)`;
-    
-    for (let i = 0; i < cellCount; i++){
+    for (let i = 0; i < cells; i++){
         const cell = document.createElement("div");
         cell.classList.add("grid-item");
-        gridContainer.appendChild(cell);
+        container.appendChild(cell);
         cell.addEventListener("mouseenter", changeCellColor);
     }
 }
 
 /*=== Start App ===
 =============================================================================*/
-createGrid();
+createGrid(gridContainer, gridDefaultColumns);
